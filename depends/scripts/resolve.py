228d4535d0a9a328e243a6ad3a9dc325792cfd79
lyylv2010@gmail.com <lyylv2010@gmail.com>
2025-10-10 19:53:15 +0800

    fmt

--- depends/scripts/resolve.py
+++ depends/scripts/resolve.py
@@ -12,7 +12,18 @@ mkdir -p cmake_build && cd cmake_build &&
 cmake -DCMAKE_INSTALL_PREFIX=$DST_PATH .. && make -j 8 && make install
 ''',
         'check_files':["lib/libsrt.a"]
+    },
+    {
+        'pkg_name':'fmt',
+        'version': '12.0.0',
+        'pkg_file':'fmt-12.0.0.tar.gz',
+        'http_download_url': 'https://github.com/fmtlib/fmt/archive/refs/tags/12.0.0.tar.gz',
+        'build_command':'''
+cd fmt-12.0.0 && mkdir -p cmake_build && cd cmake_build && cmake -DCMAKE_INSTALL_PREFIX=$DST_PATH .. && make -j 8 && make install
+''',
+        'check_files':["lib/libfmt.a"]
     }
+
 
 ]
 
